774cebc8930b6d3a73b7632ae984c50fb15d7dac
Daniil Savik <savikdaniil22@gmail.com>
2023-05-16 21:27:46 +0300

    Create form filter.

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,33 +1,129 @@
 import { NumberInput, Select } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { IconChevronDown } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 import "./Filter.css";
 
-export function Filter() {
+interface Catalogues {
+  label: string;
+  value: string;
+}
+
+export interface FormValues {
+  catalogues: Catalogues;
+  paymentFrom: string;
+  paymentTo: string;
+}
+
+interface FilterProps {
+  setFormValues: (data: FormValues) => void;
+}
+
+interface CataloguesDto {
+  title_rus: string;
+  url_rus: string;
+  title: string;
+  title_trimmed: string;
+  key: number;
+  positions: CataloguesDto[];
+}
+
+export function Filter({ setFormValues }: FilterProps) {
+  const [items, setItems] = useState<Catalogues[]>([]);
+
+  const form = useForm<FormValues>({
+    initialValues: {
+      catalogues: {
+        label: "",
+        value: "",
+      },
+      paymentFrom: "",
+      paymentTo: "",
+    },
+  });
+
+  function setCatalogues(result: CataloguesDto[]): void {
+    let dataCatalogues: Catalogues[] = [];
+
+    function mapCatalogues(result: CataloguesDto[]): void {
+      result.forEach((data: CataloguesDto): void => {
+        dataCatalogues.push({
+          label: data.title_rus || data.title || `${data.key}`,
+          value: `${data.key}`,
+        } as Catalogues);
+
+        if (data?.positions?.length > 0) {
+          mapCatalogues(data.positions);
+        }
+      });
+    }
+
+    mapCatalogues(result);
+    setItems(dataCatalogues);
+  }
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/catalogues`, {
+      headers: {
+        "x-secret-key": `${process.env.REACT_APP_SECRET_KEY}`,
+        "x-Api-App-Id": `${process.env.REACT_APP_APP_ID}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => setCatalogues(result),
+        (error) => {
+          Error(error);
+        }
+      );
+  }, []);
+
   return (
-    <div className="filter">
+    <form
+      className="filter"
+      onSubmit={form.onSubmit(() => setFormValues(form.values))}
+    >
       <div className="filter__header">
         <h1>Фильтры</h1>
-        <button>Сбросить все</button>
+        <button onClick={() => form.reset()}>Сбросить все</button>
       </div>
       <Select
+        name="catalogues"
         label="Отрасль"
         placeholder="Выберите отрасль"
         rightSection={<IconChevronDown size="1rem" />}
         rightSectionWidth={30}
         radius="md"
         size="md"
-        styles={{ rightSection: { pointerEvents: "none" } }}
-        data={[""]}
+        styles={{
+          rightSection: { pointerEvents: "none" },
+          item: { textOverflow: "ellipsis", overflow: "hidden" },
+        }}
+        data={items}
+        dropdownComponent="div"
+        maxDropdownHeight={380}
+        {...form.getInputProps("catalogues")}
       />
       <NumberInput
+        name="paymentFrom"
         placeholder="От"
         label="Оклад"
         radius="md"
         size="md"
         min={0}
+        {...form.getInputProps("paymentFrom")}
       />
-      <NumberInput placeholder="До" radius="md" size="md" min={0} />
-      <button className="filter__button-apply">Применить</button>
-    </div>
+      <NumberInput
+        name="paymentTo"
+        placeholder="До"
+        radius="md"
+        size="md"
+        min={0}
+        {...form.getInputProps("paymentTo")}
+      />
+      <button className="filter__button-apply" type="submit">
+        Применить
+      </button>
+    </form>
   );
 }
